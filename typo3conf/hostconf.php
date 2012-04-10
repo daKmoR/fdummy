62ec0a5c70575707b7bef4f59c826ec744581642
daKmoR <thomas.allmer@webteam.at>
2012-04-10 13:28:10 +0200

    example hostconf

---
+++ typo3conf/hostconf.php
@@ -0,0 +1,10 @@
+<?php
+/*
+	$typo_db_username = '<user>';
+	$typo_db_password = '<password>';
+	$typo_db_host = 'localhost';
+	$typo_db = '<database>';
+	
+	$TYPO3_CONF_VARS['GFX']['im_path'] = '/usr/bin/';
+	// ...
+*/
