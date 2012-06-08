cd388034ee8b8f991faa514b4a7afef416c88362
daKmoR <thomas.allmer@webteam.at>
2012-06-08 16:54:10 +0200

    [FEATURE] Script to update all submodules to master (except typo3_src)

---
+++ submodulesToMaster.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd typo3conf/ext/mootools_essentials
+git checkout master
+git fetch
+git rebase
+git submodule update --init --recursive
+cd ../../../
+
+cd typo3conf/ext/mootools_stack
+git checkout master
+git fetch
+git rebase
+git submodule update --init --recursive
+cd ../../../
+
+cd typo3conf/ext/tinymce_rte
+git checkout master
+git fetch
+git rebase
+git submodule update --init --recursive
+cd ../../../
+
+cd typo3conf/ext/wt_base
+git checkout master
+git fetch
+git rebase
+git submodule update --init --recursive
+cd ../../../
