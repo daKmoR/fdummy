6dc6b58cc859179e6515e0096d30cb0d4471cf72
daKmoR <thomas.allmer@webteam.at>
2012-06-08 16:43:51 +0200

    [TASK] more cleanup and use fluidtemplate

--- typo3conf/ext/site_default/Configuration/TypoScript/page.ts
+++ typo3conf/ext/site_default/Configuration/TypoScript/page.ts
@@ -1,8 +1,10 @@
 <INCLUDE_TYPOSCRIPT: source="FILE: EXT:wt_base/Configuration/TypoScript/page.ts">
+# if english is your primary language uncomment the following line
+# <INCLUDE_TYPOSCRIPT: source="FILE: EXT:wt_base/Configuration/TypoScript/page.englishMain.ts">
 
 RTE.default {
 	loadConfig = EXT:tinymce_rte/static/standard.ts
 }
 
 # use zoom enlarge as default for new content elements
-TCAdefaults.tt_content.image_zoom = 1
+# TCAdefaults.tt_content.image_zoom = 1
--- typo3conf/ext/site_default/Configuration/TypoScript/setup.ts
+++ typo3conf/ext/site_default/Configuration/TypoScript/setup.ts
@@ -6,26 +6,18 @@
 # if english is your second language (most common case) uncomment the following line
 # <INCLUDE_TYPOSCRIPT: source="FILE: EXT:wt_base/Configuration/TypoScript/setup.english.ts">
 
-## Header ##########################################################################################
-lib.header = COA
-lib.header {
-	10 = IMAGE
-	10 {
-		file = EXT:site_default/Resources/Public/Css/Images/logo.png
-		stdWrap.typolink {
-			parameter = 1
-			ATagParams = id="homeLink"
-		}
-	}
-}
+## Global ##########################################################################################
+# the Bootstrap responsive css is disabled by default
+# page.includeCSS.bootstrapResponsive >
 
+## Language ########################################################################################
+
+## Header ##########################################################################################
 lib.headerMenu < menus.nestedList
 lib.headerMenu.1.wrap = <ul class="nav lvl1">|</ul>
 lib.headerMenu.2 >
 
-## Left Content ####################################################################################
-# get first content from parentpage up to rootpage # use additional slide.collect = -1 to get all content
-lib.leftContent.50.slide = -1
+## lib.leftContent #################################################################################
 
 # Menu: default nested ul/li list
 lib.leftMenu < menus.nestedList
@@ -33,13 +25,11 @@ lib.leftMenu.entryLevel = 1
 lib.leftMenu.1.wrap = <ul class="nav nav-list lvl1">|</ul>
 lib.leftMenu.2 >
 
-## Content #########################################################################################
-# so far nothing defined
+## lib.content #####################################################################################
 
-## Right Content ###################################################################################
-# so far nothing defined
+## lib.rightContent ################################################################################
 
-## Footer ##########################################################################################
+## lib.footer ######################################################################################
 lib.footer {
 	5 = TEXT
 	5.value = WEBTEAM GmbH - Münzgrabenstrasse 36 - 8010 Graz - <a href="mailto:office@webteam.at">office@webteam.at</a>
@@ -56,12 +46,3 @@ lib.footerMenu.special.value = 4
 # [PIDinRootline = 13]
 # options for subpages only (list possible)
 # [PIDupinRootline = 13,37]
-
-[globalVar = TSFE:id = 6]
-	<INCLUDE_TYPOSCRIPT: source="FILE: EXT:xform/Configuration/TypoScript/setup.txt">
-	plugin.tx_xform {
-		view {
-			#templateRootPath = fileadmin/templates/default/xform/
-		}
-	}
-[global]
