27673182222ec97dc7c5034ed9e9bd90c512c6ab
daKmoR <thomas.allmer@webteam.at>
2012-09-25 12:29:07 +0200

    [FEATURE] examples for the deploy script

--- Build/Surf/#useMe.php
+++ Build/Surf/#useMe.php
@@ -2,15 +2,15 @@
 	$workflow = new \TYPO3\Surf\Domain\Model\SimpleWorkflow();
 	$deployment->setWorkflow($workflow);
 
-	$node = new \TYPO3\Surf\Domain\Model\Node('<name>');
-	$node->setHostname('<hostname>');
-	$node->setOption('username', '<username>');
+	$node = new \TYPO3\Surf\Domain\Model\Node('<name>'); //examples clients: webteamatClientsWebteamAt live: webteamAt
+	$node->setHostname('<hostname>'); //examples clients: clients.webteam.at live: webteam.at
+	$node->setOption('username', '<username>'); //examples clients: webteamat live: userabc
 	$node->setOption('password', '<password>');
 	//$node->setOption('port', '26');
 
 	$application = new \TYPO3\Surf\Application\TYPO3v6();
-	$application->setDeploymentPath('<pathWithOutTrailingSlash>');
-	$application->setOption('repositoryUrl', '<fullGitUrl>');
+	$application->setDeploymentPath('<pathWithOutTrailingSlash>'); //examples clients: /home/webteamat live: /home/userabc/www/
+	$application->setOption('repositoryUrl', '<fullGitUrl>'); //example: git@git.webteam.at:clients/webteamat.git
 	$application->setOption('transferMethod', 'rsync');
 	$application->setOption('keepReleases', 2);
 	$application->addNode($node);
